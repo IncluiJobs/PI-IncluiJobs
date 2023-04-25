4b76033f6509dd614366fc2da8b16b148e2ae01b
VinnyPC <vinisilvax3@gmail.com>
2023-04-25 12:46:16 -0300

    Adicionado MVC de postagem e CRUD

--- incluijobs/src/main/java/com/generation/incluijobs/model/Tema.java
+++ incluijobs/src/main/java/com/generation/incluijobs/model/Tema.java
@@ -1,9 +1,16 @@
 package com.generation.incluijobs.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,6 +29,12 @@ public class Tema {
     @NotBlank(message = "A identificação do grupo é Obrigatório!")
     @Size(max = 255, message = "A identificação do grupo deve conter no máximo 255 caracteres")
     private String grupo;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagem;
+
+    
 
 	public Long getId() {
 		return id;
@@ -46,6 +59,19 @@ public class Tema {
 	public void setGrupo(String grupo) {
 		this.grupo = grupo;
 	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+	
+	
+	
 
    
 }
